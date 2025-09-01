6920ac2778f4f1a96df0af0bf3b70d25773a8801
SantiCaser-xp <santino.caserta@davinci.edu.ar>
2025-09-01 18:56:33 -0300

    Fixed scripts

--- Assets/Scripts/PlacementState.cs
+++ Assets/Scripts/PlacementState.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
-public class PlacementState : MonoBehaviour
+public class PlacementState : IBuildingState
 {
     private int selectedObjectIndex = -1;
     int ID;
@@ -12,4 +13,70 @@ public class PlacementState : MonoBehaviour
     GridData floorData;
     GridData structureData;
     ObjectPlacer objectPlacer;
+
+    public PlacementState(int iD,
+                          Grid grid,
+                          PreviewSystem previewSystem,
+                          ObjectsDatabaseSO dataBase,
+                          GridData floorData,
+                          GridData structureData,
+                          ObjectPlacer objectPlacer)
+    {
+        ID = iD;
+        this.grid = grid;
+        this.previewSystem = previewSystem;
+        this.dataBase = dataBase;
+        this.floorData = floorData;
+        this.structureData = structureData;
+        this.objectPlacer = objectPlacer;
+
+        selectedObjectIndex = dataBase.objectsData.FindIndex(obj => obj.ID == ID);
+        if (selectedObjectIndex > -1)
+        {
+            previewSystem.StartShowingPlacementPreview(dataBase.objectsData[selectedObjectIndex].Prefab, dataBase.objectsData[selectedObjectIndex].Size);
+        }
+        else
+        {
+            throw new System.Exception("Object with ID " + iD + " not found in database.");
+        }
+    }
+
+    public void EndState()
+    {
+        previewSystem.StopShowingPreview();
+    }
+
+    public void OnAction(Vector3Int gridPosition)
+    {
+        bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+        if (placementValidity == false)
+        {
+            return;
+        }
+
+        int index = objectPlacer.PlaceObject(dataBase.objectsData[selectedObjectIndex].Prefab, grid.CellToWorld(gridPosition));
+
+
+        GridData selectedData = dataBase.objectsData[selectedObjectIndex].ID == 0 ?
+            floorData :
+            structureData;
+        selectedData.AddObject(gridPosition, dataBase.objectsData[selectedObjectIndex].Size, dataBase.objectsData[selectedObjectIndex].ID, index);
+        previewSystem.UpdatePosition(grid.CellToWorld(gridPosition), false);
+    }
+
+    private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjectIndex)
+    {
+        GridData selectedData = dataBase.objectsData[selectedObjectIndex].ID == 0 ?
+            floorData :
+            structureData;
+
+        return selectedData.CanPlaceObject(gridPosition, dataBase.objectsData[selectedObjectIndex].Size);
+    }
+
+    public void UpdateState(Vector3Int gridPosition)
+    {
+        bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+
+        previewSystem.UpdatePosition(grid.CellToWorld(gridPosition), placementValidity);
+    }
 }
--- Assets/Scripts/PlacementSystem.cs
+++ Assets/Scripts/PlacementSystem.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class PlacementSystem : MonoBehaviour
 {
-    [SerializeField] GameObject mouseIndicator;
 
     [SerializeField] InputManager inputManager;
 
@@ -13,7 +12,6 @@ public class PlacementSystem : MonoBehaviour
 
     [SerializeField]
     private ObjectsDatabaseSO database;
-    private int selectedObjectIndex = -1;
 
     [SerializeField] private GameObject gridVisualization;
 
@@ -27,6 +25,8 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField]
     private ObjectPlacer objectPlacer;
 
+    IBuildingState buildingState;
+
     private void Start()
     {
         StopPlacement();
@@ -37,14 +37,8 @@ public class PlacementSystem : MonoBehaviour
     public void StartPlacement(int ID)
     {
         StopPlacement();
-        selectedObjectIndex = database.objectsData.FindIndex(obj => obj.ID == ID);
-        if (selectedObjectIndex < 0)
-        {
-            Debug.LogError("Object with ID " + ID + " not found in database.");
-            return;
-        }
         gridVisualization.SetActive(true);
-        preview.StartShowingPlacementPreview(database.objectsData[selectedObjectIndex].Prefab, database.objectsData[selectedObjectIndex].Size);
+        buildingState = new PlacementState(ID, grid, preview, database, floorData, structureData, objectPlacer);
         inputManager.OnClicked += PlaceStructure;
         inputManager.OnExit += StopPlacement;
     }
@@ -58,44 +52,35 @@ public class PlacementSystem : MonoBehaviour
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
 
-        bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
-        if (placementValidity == false)
-        {
-            return;
-        }
-
-        int index = objectPlacer.PlaceObject (database.objectsData[selectedObjectIndex].Prefab, grid.CellToWorld(gridPosition));
-
-
-        GridData selectedData = database.objectsData[selectedObjectIndex].ID == 0 ?
-            floorData :
-            structureData;
-        selectedData.AddObject(gridPosition, database.objectsData[selectedObjectIndex].Size, database.objectsData[selectedObjectIndex].ID, index);
-        preview.UpdatePosition(grid.CellToWorld(gridPosition), false);
+        buildingState.OnAction(gridPosition);
     }
 
-    private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjectIndex)
-    {
-        GridData selectedData = database.objectsData[selectedObjectIndex].ID == 0 ?
-            floorData :
-            structureData;
+    //private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjectIndex)
+    //{
+    //    GridData selectedData = database.objectsData[selectedObjectIndex].ID == 0 ?
+    //        floorData :
+    //        structureData;
 
-        return selectedData.CanPlaceObject(gridPosition, database.objectsData[selectedObjectIndex].Size);
-    }
+    //    return selectedData.CanPlaceObject(gridPosition, database.objectsData[selectedObjectIndex].Size);
+    //}
 
     private void StopPlacement()
     {
-        selectedObjectIndex = -1;
+        if (buildingState == null)
+        {
+            return;
+        }
         gridVisualization.SetActive(false);
-        preview.StopShowingPreview();
+        buildingState.EndState();
         inputManager.OnClicked -= PlaceStructure;
         inputManager.OnExit -= StopPlacement;
         lastDetectedPosition = Vector3Int.zero;
+        buildingState = null;
     }
 
     private void Update()
     {
-        if (selectedObjectIndex < 0)
+        if (buildingState == null)
         {
             return;
         }
@@ -104,10 +89,7 @@ public class PlacementSystem : MonoBehaviour
 
         if (lastDetectedPosition != gridPosition)
         {
-            bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
-
-            mouseIndicator.transform.position = mousePosition;
-            preview.UpdatePosition(grid.CellToWorld(gridPosition), placementValidity);
+            buildingState.UpdateState(gridPosition);
             lastDetectedPosition = gridPosition;
         }
     }
