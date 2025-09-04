a74111d4cf8ed632be98050125f0e45a1cc34858
SantiCaser-xp <santino.caserta@davinci.edu.ar>
2025-09-04 00:15:23 -0300

    Fixed grid positioning

--- Assets/Scripts/I_TestDamageable.cs
+++ Assets/Scripts/I_TestDamageable.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class I_TestDamageable : MonoBehaviour
+public interface I_TestDamageable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void TakeDamage(int damageAmount);
 }
--- Assets/Scripts/TestEnemy.cs
+++ Assets/Scripts/TestEnemy.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestEnemy : MonoBehaviour
+public class TestEnemy : MonoBehaviour, I_TestDamageable
 {
     private Rigidbody rb;
+    [SerializeField] private float hp = 100f;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -15,5 +17,14 @@ public class TestEnemy : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + Vector3.left * Time.fixedDeltaTime * 2);
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        hp -= damageAmount;
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
