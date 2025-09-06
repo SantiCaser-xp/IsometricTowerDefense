45b64317351a16668e0847ba0febbc5be13beba7
Gilespie <pierreraider@gmail.com>
2025-09-06 03:30:22 -0300

    Added Camera follower
    Added script CameraFollower(follow for character).

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField] private Character _character;
+    [SerializeField] private float _speedRate;
+    [SerializeField] private Vector3 _offset;
+
+    private void Start()
+    {
+        transform.position = _character.transform.position + _offset;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPos = _character.transform.position + _offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, _speedRate * Time.fixedDeltaTime);
+    }
+}
