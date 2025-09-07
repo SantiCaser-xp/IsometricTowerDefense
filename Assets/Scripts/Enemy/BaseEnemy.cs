50779228a82517080fb82e0122fa592da7be847f
Alexey_Roslyakov <Alexey.roslyakov@davinci.edu.ar>
2025-09-07 20:18:51 -0300

    feat (enemy): NavMesh pathfinding and chose target

--- Assets/Scripts/Enemy/BaseEnemy.cs
+++ Assets/Scripts/Enemy/BaseEnemy.cs
@@ -1,17 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class BaseEnemy : MonoBehaviour
+public class BaseEnemy : MonoBehaviour, I_TestDamageable
 {
-    [SerializeField] Transform[] waypoints;
-    [SerializeField] int currentWaypoint;
-
+    [Header("Enemy Stats")]
+    [SerializeField] protected float health = 100f;
+    [SerializeField] protected float damage = 10f;
+    [SerializeField] public float attackRange = 2f;
+    [SerializeField] public float attackCooldown = 1f;
+    [SerializeField] public float searchInterval = 0.5f;
     [SerializeField] float walkSpeed;
     [SerializeField] float idleTime;
     [SerializeField] public float detectionRadius;
     [SerializeField] public LayerMask targetMask;
     [SerializeField] public float _energy;
+
+    [Header("Targeting")]
+    [SerializeField] protected TargetingStrategy targetingStrategy = TargetingStrategy.Nearest;
+    public  ITargetable currentTarget;
+    [SerializeField] Transform[] waypoints;
+    [SerializeField] int currentWaypoint;
+    public float lastAttackTime;
+    public float lastSearchTime;
+
+
+
+    [Header("Components")]
+    public NavMeshAgent agent;
+    protected Animator animator;
+
+    public event Action<I_TestDamageable> OnDeath;
+
+
     public GameObject _target
     {
         get; private set;
@@ -30,6 +53,11 @@ public class BaseEnemy : MonoBehaviour
 
     private void Awake()
     {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        EnemyManager.Instance?.RegisterEnemy(this);
+
         _enemyFSM = new EnemyFSM<EnemyFSMStates, BaseEnemy>();
 
         _enemyFSM._possibleStates.Add(EnemyFSMStates.Idle, new IdleState().SetUp(_enemyFSM).SetAvatar(this));
@@ -51,38 +79,100 @@ public class BaseEnemy : MonoBehaviour
         {
             _enemyFSM.ChangeState(EnemyFSMStates.Move);
         }
+        
+    }
+    
+
+    #region Targeting
+    public virtual void SearchForTarget()
+    {
+        currentTarget = EnemyTargetManager.Instance?.GetOptimalTarget(transform.position, targetingStrategy);
+    }
+    public void NotifyTargetLost(ITargetable lostTarget)
+    {
+        if (currentTarget == lostTarget)
+        {
+            currentTarget = null;
+            // if (_enemyFSM._actualState() == EnemyFSMStates.Die) { }   
+
+            _enemyFSM.ChangeState(EnemyFSMStates.Idle);
+        }
+    }
+    #endregion
+    #region Combat
+    public virtual void PerformAttack()
+    {
+        
+        if (currentTarget != null && currentTarget.IsAlive)
+        {
+            currentTarget.TakeDamage(damage);
+            Debug.Log($"PerformAttack with {damage}");
+        }
+    }
+    public void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+
+
+
+    public virtual void Die()
+    {
+        _enemyFSM.ChangeState(EnemyFSMStates.Die);
+
+        OnDeath?.Invoke(this);
+        Destroy(gameObject, 2f);
 
     }
-    public bool ReduceEnergy(float fatigue)
+
+    protected virtual void OnDestroy()
     {
-        _energy -= fatigue;
-        if (_energy <= 0)
-        {
-            _energy = 0;
-            return true;
-        }
-        return false;
+        EnemyManager.Instance?.UnregisterEnemy(this);
     }
-    public void RecoverEnergy()
-    {
-        _energy = 100;
-    }
-    public bool MoveTo(int index)
-    {
-        var dirVector = waypoints[index].position - transform.position;
-        transform.position += dirVector.normalized * Time.deltaTime * walkSpeed;
-        if (dirVector.magnitude < 0.2f)
-        {
-            return true;
-        }
-        return false;
-    }
-    public void SetTarget(GameObject target)
-    {
-        _target = target;
-    }
+    #endregion
+
+    // Energy logic - skip for now
+    //public bool ReduceEnergy(float fatigue)
+    //{
+    //    _energy -= fatigue;
+    //    if (_energy <= 0)
+    //    {
+    //        _energy = 0;
+    //        return true;
+    //    }
+    //    return false;
+    //}
+    //public void RecoverEnergy()
+    //{
+    //    _energy = 100;
+    //}
+    //public bool MoveTo(int index)
+    //{
+    //    var dirVector = waypoints[index].position - transform.position;
+    //    transform.position += dirVector.normalized * Time.deltaTime * walkSpeed;
+    //    if (dirVector.magnitude < 0.2f)
+    //    {
+    //        return true;
+    //    }
+    //    return false;
+    //}
+    //public void SetTarget(GameObject target)
+    //{
+    //    _target = target;
+    //}
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        
     }
+
+    //void I_TestDamageable.TakeDamage(int damageAmount)
+    //{
+    //    TakeDamage(damageAmount);
+    //}
 }
--- Assets/Scripts/Enemy/Enemy States/AttackState.cs
+++ Assets/Scripts/Enemy/Enemy States/AttackState.cs
@@ -7,13 +7,34 @@ public class AttackState : EnemyState<EnemyFSMStates, BaseEnemy>
     public override void OnEnter()
     {
         Debug.Log($"Debug Enter a AttackState");
+        avatar.agent.isStopped = true;
     }
 
     public override void OnExecute()
     {
-        if (Input.GetKeyDown(KeyCode.V))
+        if (avatar.currentTarget == null || !avatar.currentTarget.IsAlive)
         {
-            enemyFSM.ChangeState(EnemyFSMStates.Die);
+            enemyFSM.ChangeState(EnemyFSMStates.Idle);
+            return;
+        }
+
+        float distanceToTarget = Vector3.Distance(avatar.transform.position, avatar.currentTarget.Position);
+        if (distanceToTarget > avatar.attackRange)
+        {
+            enemyFSM.ChangeState(EnemyFSMStates.Move);
+            return;
+        }
+        // Turning towards the target
+        Vector3 lookDirection = (avatar.currentTarget.Position - avatar.transform.position).normalized;
+        lookDirection.y = 0;
+        avatar.transform.rotation = Quaternion.LookRotation(lookDirection);
+
+        // Attack with cooldown
+        if (Time.time - avatar.lastAttackTime >= avatar.attackCooldown)
+        {
+            Debug.Log($"Attack with cooldown");
+            avatar.lastAttackTime = Time.time;
+            avatar.PerformAttack();
         }
     }
     public override void OnExit()
