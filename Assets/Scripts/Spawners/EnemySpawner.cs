1e7121131ae9cde360aba761ff87ae7a0cfec61f
SantiCaser-xp <santino.caserta@davinci.edu.ar>
2025-10-02 18:11:32 -0300

    Implemented cloud keys

--- Assets/Scripts/Spawners/EnemySpawner.cs
+++ Assets/Scripts/Spawners/EnemySpawner.cs
@@ -1,10 +1,31 @@
+using Unity.Services.RemoteConfig;
 using UnityEngine;
 
 public class EnemySpawner : GenericSpawner<EnemyFactory>
 {
+    public bool spawnEnemy;
     protected override void Spawn()
     {
         var enemy = _factory.Create();
         enemy.transform.position = _spawnPoints[0].position;
+    }
+
+    private void Start()
+    {
+        RemoteConfigService.Instance.FetchCompleted += UpdateData;
+    }
+
+    public void UpdateData(ConfigResponse configResponse)
+    {
+        spawnEnemy = RemoteConfigService.Instance.appConfig.GetBool("SpawnEnemy");
+
+        if (spawnEnemy)
+        {
+            InvokeRepeating(nameof(Spawn), 0, _secondToSpawn);
+        }
+        else
+        {
+            CancelInvoke(nameof(Spawn));
+        }
     }
 }
--- Assets/Scripts/StickScaleManager.cs
+++ Assets/Scripts/StickScaleManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Services.RemoteConfig;
 using UnityEngine;
 
 public class StickScaleManager : MonoBehaviour
 {
-    public float newScale = 1f;
+    public float myScale = 1f;
+    private float targetScale;
+    private float lerpSpeed = 5f; // Puedes ajustar la velocidad
+
     void Start()
     {
-        
+        RemoteConfigService.Instance.FetchCompleted += UpdateData;
+        targetScale = myScale;
+        transform.localScale = new Vector3(myScale, myScale, myScale);
     }
 
-    // Update is called once per frame
-    void UpdateData()
+    public void UpdateData(ConfigResponse configResponse)
     {
-        
+        targetScale = RemoteConfigService.Instance.appConfig.GetFloat("StickScale");
+        myScale = transform.localScale.x;
+        StartCoroutine(ChangeScale());
+        // No se cambia directamente la escala aquí
+    }
+
+    public IEnumerator ChangeScale()
+    {
+        Debug.Log("Cambiando escala a: " + targetScale);
+        while (Mathf.Abs(transform.localScale.x - targetScale) > 0.01f)
+        {
+            float newScale = Mathf.Lerp(transform.localScale.x, targetScale, Time.deltaTime * lerpSpeed);
+            transform.localScale = new Vector3(newScale, newScale, newScale);
+            yield return null;
+        }
+        // Asegura que la escala final sea exacta
+        transform.localScale = new Vector3(targetScale, targetScale, targetScale);
     }
 }
