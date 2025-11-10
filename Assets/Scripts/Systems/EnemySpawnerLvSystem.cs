072dd599f1e53d6d3c70bb2abab9a84de2b985ca
SantiCaser-xp <santino.caserta@davinci.edu.ar>
2025-11-10 19:32:06 -0300

    Working on enemy waves

--- Assets/Scripts/Systems/EnemySpawnerLvSystem.cs
+++ Assets/Scripts/Systems/EnemySpawnerLvSystem.cs
@@ -4,6 +4,46 @@ using UnityEngine;
 
 public class EnemySpawnerLvSystem : MonoBehaviour
 {
-    [SerializeField] private GameObject _sign;
+    [SerializeField] private Animator _sign;
 
+    [SerializeField] private EnemyFactory _lowEnemyFactory;
+    [SerializeField] private EnemyFactory _midEnemyFactory;
+    [SerializeField] private EnemyFactory _hardEnemyFactory;
+
+    [SerializeField] private Transform _upZone;
+    [SerializeField] private Transform _downZone;
+    [SerializeField] private Transform _rightZone;
+    [SerializeField] private Transform _leftZone;
+
+    [SerializeField] private Transform _currentZone;
+
+    [SerializeField] private int _enemiesToSpawn;
+
+
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+    public void Spawn(EnemyFactory currFactory)
+    {
+        var enemy = currFactory.Create();
+        //enemy.transform.position = _currentZone.position;
+        enemy.transform.position = new Vector3 (_currentZone.position.x + Random.Range(3,6),
+            _currentZone.position.y,
+            _currentZone.position.z + Random.Range(3, 6));
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        yield return new WaitForSeconds(3f);
+        _sign.SetTrigger("RingRight");
+        _currentZone = _rightZone;
+        _enemiesToSpawn = 5;
+        for (int i = 0; i < _enemiesToSpawn; i++)
+        {
+            Spawn(_lowEnemyFactory);
+            yield return new WaitForSeconds(1f);
+        }
+    }
 }
