383796e66d4df15ea5cd531d862a1429de5e78cc
SantiCaser-xp <santino.caserta@davinci.edu.ar>
2025-11-25 01:27:35 -0300

    Fix turrets

--- Assets/Scripts/Towers/AbstractTower.cs
+++ Assets/Scripts/Towers/AbstractTower.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public abstract class AbstractTower : Destructible
@@ -10,7 +11,8 @@ public abstract class AbstractTower : Destructible
     [SerializeField] protected bool isDead = false;
     [SerializeField] protected LayerMask _enemyMask;
     protected float fireCountdown = 0f;
-    protected List<IDamageable<float>> enemiesInRange = new List<IDamageable<float>>();
+    [SerializeField] protected List<IDamageable<float>> enemiesInRange = new List<IDamageable<float>>();
+    //[SerializeField] protected List<GameObject> enemiesInRange = new List<GameObject>();
 
     protected virtual void Awake()
     {
@@ -32,8 +34,35 @@ public abstract class AbstractTower : Destructible
     protected virtual void SphereCasting()
     {
         Collider[] hits = Physics.OverlapSphere(transform.position, radius, _enemyMask);
-        HashSet<IDamageable<float>> detectedEnemies = new HashSet<IDamageable<float>>();
+        //HashSet<IDamageable<float>> detectedEnemies = new HashSet<IDamageable<float>>();
 
+        //foreach (var hit in hits)
+        //{
+        //    var enemy = hit.GetComponent<IDamageable<float>>();
+
+        //    if (enemy != null)
+        //    {
+        //        //detectedEnemies.Add(enemy);
+        //        if (!enemiesInRange.Contains(enemy))
+        //        {
+        //            enemiesInRange.Add(enemy);
+        //            hit.gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+        //        }
+        //    }
+        //}
+
+        //for (int i = enemiesInRange.Count - 1; i >= 0; i--)
+        //{
+        //    var enemy = enemiesInRange[i] as MonoBehaviour;
+        //    if (enemy == null || !enemy.gameObject.activeInHierarchy /*|| !detectedEnemies.Contains(enemiesInRange[i])*/)
+        //    {
+        //        enemiesInRange.RemoveAt(i);
+        //        enemy.gameObject.GetComponent<Renderer>().material.color = Color.white;
+
+        //    }
+        //}
+        enemiesInRange.Clear();
 
         foreach (var hit in hits)
         {
@@ -41,18 +70,12 @@ public abstract class AbstractTower : Destructible
 
             if (enemy != null)
             {
-                detectedEnemies.Add(enemy);
-                if (!enemiesInRange.Contains(enemy))
+                if (Vector3.Distance(transform.position, hit.transform.position) < radius)
+                {
+                    hit.gameObject.GetComponent<Renderer>().material.color = Color.red;
+                    Debug.Log(Vector3.Distance(transform.position, hit.transform.position));
                     enemiesInRange.Add(enemy);
-            }
-        }
-
-        for (int i = enemiesInRange.Count - 1; i >= 0; i--)
-        {
-            var enemy = enemiesInRange[i] as MonoBehaviour;
-            if (enemy == null || !enemy.gameObject.activeInHierarchy || !detectedEnemies.Contains(enemiesInRange[i]))
-            {
-                enemiesInRange.RemoveAt(i);
+                }
             }
         }
     }
